9dc5a620a25adb9defae93b0349a993413f749a6
Phyo Min Oo <109222720+phyominoo27@users.noreply.github.com>
2024-02-25 18:10:23 +0630

    Create Sketch game Add black and random color marker (also a Eraser) can select between 2 to 100 Grid

---
+++ script.js
@@ -0,0 +1,62 @@
+const grid = document.querySelector(".grid-container");
+const reset = document.querySelector(".reset");
+const warning = document.querySelector(".warning");
+
+let color = "black";
+
+function gridLayout(size) {
+  while (grid.hasChildNodes()) {
+    grid.removeChild(grid.firstChild);
+  }
+  for (let i = 0; i < size * size; i++) {
+    grid.appendChild(createGrid(800 / size));
+  }
+  paint()
+}
+
+function changeSize(input) {
+  if (input >= 2 && input <= 100) {
+    gridLayout(input);
+  } else {
+    const warningMsg = document.createElement("div");
+    warningMsg.textContent = "Please only enter a number between 2 and 100. ";
+    warning.appendChild(warningMsg);
+  }
+}
+
+function createGrid(divsize) {
+  const divChild = document.createElement("div");
+  divChild.classList.add("grid-item");
+  divChild.style.width = divChild.style.height = `${divsize}px`;
+  return divChild;
+}
+
+function paint() {
+  const allGridSelect = document.querySelectorAll(".grid-item");
+  allGridSelect.forEach((gridItem) => {
+    gridItem.addEventListener("mouseover", () => {
+      if (color === "random") {
+        gridItem.style.backgroundColor = `hsl(${
+          Math.random() * 360
+        }, 100%, 50%)`;
+      } else {
+        gridItem.style.backgroundColor = color;
+      }
+    });
+  });
+}
+
+function changeColor(input) {
+  color = input;
+}
+
+
+
+reset.addEventListener("click", () => {
+  while (grid.hasChildNodes()) {
+    grid.removeChild(grid.firstChild);
+  }
+  gridLayout(16);
+});
+
+gridLayout(16);
